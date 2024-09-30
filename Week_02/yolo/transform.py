63ec3477f08b05c14def93c6e80d359e2f88028c
soyun <ksy010627@yonsei.ac.kr>
2024-10-01 01:30:18 +0900

    del

--- Week_02/yolo/transform.py
+++
@@ -1,8 +0,0 @@
-class Compose(object):
-    def __init__(self, transforms):
-        self.transforms = transforms
-
-    def __call__(self, img, bboxes):
-        for t in self.transforms:
-            img, bboxes = t(img), bboxes
-        return img, bboxes
